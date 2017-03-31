ed9a1f4452501291e434ac7600e3111de170eb5b
Mario Garcia <magarcia@verisk.com>
2017-03-31 07:38:11 -0600

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,26 @@
+import * as angular from 'angular';
+
+import '../style/app.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  url: string;
+  constructor() {
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
